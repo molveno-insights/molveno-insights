dfd5d3db1594c8feccbb2864bf1a4bef54ef1aeb
andriesaarts1986 <andries_aarts1986@hotmail.com>
2020-02-19 15:50:25 +0100

    Deleted guest profiles link

--- resources/views/layout/adminbase.blade.php
+++ resources/views/layout/adminbase.blade.php
@@ -40,7 +40,14 @@
           <a class="nav-link" href="#">Categories</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Logout</a>
+          <a class="nav-link" href="{{ route('logout') }}"
+             onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
         </li>
 
     </ul>
