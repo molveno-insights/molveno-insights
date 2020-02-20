cbd863474206385e708e416313a67f339814d2d4
Fides Torres <fides.torres@outlook.com>
2020-02-20 10:38:19 +0100

    Styling fix Login

--- resources/views/layout/adminbase.blade.php
+++ resources/views/layout/adminbase.blade.php
@@ -10,68 +10,65 @@
 </head>
 
 <body>
-    <p><h3>Hotel staff</h3><br>
-
+    @guest
+    <h3>Hotel staff</h3>
     <div>
-
+    @else
     <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
           <img src="/statics/Molveno_logo_white.png" height="50px">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
                 <span class="sr-only">(current)</span>
               </a>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('media.index') }}">Videos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Categories</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('logout') }}"
-             onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();">
-              {{ __('Logout') }}
-          </a>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              @csrf
-          </form>
-        </li>
-
-    </ul>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('media.index') }}">Videos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+              </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    @endguest
+      <div class="container-fluid">
+        @yield('content')
+      </div>
     </div>
-  </div>
-</nav>
-
-<div class="container-fluid">
-
-
-    @yield('content')
-<br>
-</div>
 
 <!-- Footer -->
-<footer class="page-footer font-small blue pt-4">
+  <footer class="page-footer font-small blue pt-4">
 
   <!-- Footer Links -->
-  <div class="container-fluid text-center text-md-left">
+    <div class="container-fluid text-center text-md-left">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© Molveno Lake Resort 2020 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> MolvenoLakeResort.it</a>
-  </div>
+      <div class="footer-copyright text-center py-3">© Molveno Lake Resort 2020 Copyright:
+        <a href="https://mdbootstrap.com/education/bootstrap/">MolvenoLakeResort.it</a>
+      </div>
   <!-- Copyright -->
-
-</footer>
+    </div>
+  </footer>
 <!-- Footer -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
