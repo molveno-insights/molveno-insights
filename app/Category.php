4505d26af3285afa55693f76578c7cd833857710
Marc Theunissen <mtheunissen82@gmail.com>
2020-03-12 11:17:20 +0100

    Add sport category to guest landing page

--- app/Category.php
+++ app/Category.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'category';
-    //
+
+    public function media()
+    {
+        return $this->hasMany(Media::class);
+    }
+
+    public function getRatingPercentage() {
+        return 0;
+    }
 }
