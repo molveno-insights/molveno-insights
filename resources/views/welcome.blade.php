1a2a8802de54de595d8bd992577cd1e13a238356
Fides Torres <fides.torres@outlook.com>
2020-03-12 13:42:06 +0100

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,69 +4,27 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Anton|Archivo+Black|PT+Sans+Caption&display=swap" rel="stylesheet">
-        {{-- <link rel="stylesheet" href="/css/app.css" /> --}}
-        <link rel="stylesheet" href="/css/landingpage.css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Anton|Archivo+Black|PT+Sans+Caption&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/landingpage.css" />
         <title>Molveno Lake Resort</title>
     </head>
     <body>
-
-            <section class="landingpage">
-
-                            <div>
-                                <a class="navbar-brand" href="{{ route('welcome') }}">
-                                    <img src="/statics/Molveno_logo_white.png" height="50px" />
-                                </a>
-                            </div>
-                    </div>
-
-                    <div class="col-12 text-center">
-                        <input type="text" placeholder="Search..">
-                    </div>
-
-                    <div class="col-12 hero-text text-center">
-                        <h1>Molveno Insights</h1>
-                        <p class="lead">Get to know more about Molveno Area with selected inspiring videos</p>
-                        <a href="{{ route('videopage') }}"><button>Watch Videos</button></a>
-                    </div>
-                    <div class="col-12 text-center">
-                        <a href="#text"><i class="fas fa-chevron-circle-down fa-4x"></i></a>
-                    </div>
-                </div>
-            </section>
-
-        <h2>Sport</h2>
-        <div class="container-fluid p-0 flex-center position-ref full-height">
-
-            <div class="row">
-                @foreach ($sportMedia as $sportMed)
-                <div class="col-sm-12 col-md-4">
-                    <div class="card shadow">
-
-                        <img class="card-img-top" src="https://i3.ytimg.com/vi/{{ $sportMed->url }}/hqdefault.jpg" />
-                        <div class="card-body">
-                         <h5 class="card-title"><a href="https://www.youtube.com/watch_popup?v={{ $sportMed->url }}">{{ Illuminate\Support\Str::limit($sportMed->name, 45) }}</a></h5>
-
-                            <div class="col-12">
-                                <i class="media-like fas fa-thumbs-up fa-3x" data-type="like" data-media-id="{{ $sportMed->id }}"></i>
-                                <i class="media-dislike fas fa-thumbs-down fa-3x" data-type="dislike" data-media-id="{{ $sportMed->id }}"></i>
-                            </div>
-                            <div class="col-12">
-                                (<span id="media-like-count-{{ $sportMed->id }}">{{ $sportMed->likes }}</span>)
-                                (<span id="media-dislike-count-{{ $sportMed->id }}">{{ $sportMed->dislikes }}</span>)
-                            </div>
-                            <div class="col-12">
-                                <span>{{ $sportMed->getRatingPercentage() }}% vond deze film leuk</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
+        <section class="landingpage">
+            <div>
+                <a class="navbar-brand" href="{{ route('welcome') }}">
+                    <img src="/statics/Molveno_logo_white.png" height="50px" />
+                </a>
             </div>
-        </div>
-
+            <div class="col-12 hero-text text-center">
+                <h1>Molveno Insights</h1>
+                <p class="lead">Get to know more about Molveno Area with selected inspiring videos</p>
+                <a href="{{ route('videopage') }}"><button>Watch Videos</button></a>
+            </div>
+            <div class="col-12 text-center">
+                <a href="#text"><i class="fas fa-chevron-circle-down fa-4x"></i></a>
+            </div>
+        </section>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/@popperjs/core@2.0.6/dist/umd/popper.min.js" integrity="sha384-ilN5ySyBtvpP8fGWj3u3gWBvtCbT5l60hHYMGsr0ct8wK0sy8JQRQfLYMrZ9hhI2" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
