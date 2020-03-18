219dd7772c91209c24724b5ceea663de884270ec
Fides Torres <fides.torres@outlook.com>
2020-03-18 21:45:51 +0100

    jshint

--- public/js/app.js
+++ public/js/app.js
@@ -19,14 +19,14 @@ $('input#url').on('input',(e)=>{
     if(ytId.length === 11 ){
         $(e.target).removeClass('is-invalid');
 
-        $('#yt_preview').show().attr('src','https://www.youtube.com/embed/'+ytId)
+        $('#yt_preview').show().attr('src','https://www.youtube.com/embed/'+ytId);
     }else if(ytId === '' || ytId.length < 11 ){
         $(e.target).addClass('is-invalid');
         $('#yt_preview').hide();
     }else{
         if(ytId.split('watch?v=')[1]){
             $(e.target).val(ytId.split('watch?v=')[1]);
-            $('#yt_preview').show().attr('src','https://www.youtube.com/embed/'+ytId.split('watch?v=')[1])
+            $('#yt_preview').show().attr('src','https://www.youtube.com/embed/'+ytId.split('watch?v=')[1]);
             $(e.target).removeClass('is-invalid');
         }
     }
