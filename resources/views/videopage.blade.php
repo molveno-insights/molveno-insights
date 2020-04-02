03bf71fc510f82b8dcea90f5533ecf38f6dcae48
ccolombijn <ccolombijn@gmail.com>
2020-04-02 11:30:43 +0200

    Update

--- resources/views/videopage.blade.php
+++ resources/views/videopage.blade.php
@@ -27,9 +27,8 @@
         </div>
         <div id="searchMedia" class="form-inline d-flex justify-content-center md-form form-sm"><i class="fas fa-search fa-2x"></i><input class="form-control" id="searchMediaInput" placeholder="Search media" style="display:none;" /></div>
         @if ($message ?? '' or session('message'))
-            <br>
             <div class="alert alert-primary" id="contact_alert" role="alert" style="margin:25px;font-size:1.5em;">
-                {{ $message ?? '' }}{{ session('message') }}
+                {{ $message ?? '' }}{!! session('message') !!}
             </div>
         @endif
         <div id="mediaSearchContainer" style="display:none;" class="container-fluid flex-center position-ref full-height">
